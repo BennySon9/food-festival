a681a1c2aa5e937f42589638ce3d99be358aaa97
Brandon Benson <brandonbenson09@gmail.com>
2022-08-13 19:03:50 -0400

    sdf

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,30 @@
-const path = require("path");
 const webpack = require("webpack");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const path = require("path");
 
-module.exports = {
-  entry: "./assets/js/script.js",
+const config = {
+  entry: {
+    app: "./assets/js/script.js",
+    events: "./assets/js/events.js",
+    schedule: "./assets/js/schedule.js",
+    tickets: "./assets/js/tickets.js",
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "main.bundle.js",
+    filename: "[name].bundle.js",
   },
   plugins: [
     new webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery",
     }),
+    // the report outputs to an HTML file in the DIST folder
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static",
+    }),
   ],
   mode: "development",
 };
+
+module.exports = config;
